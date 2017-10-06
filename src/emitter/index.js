e3a25eeee0ce569fabeb66385315072375755598
Eduardo Astolfi <eduardo.astolfi@prima-solutions.com>
2017-10-06 16:21:52 +0200

    REMOVE

--- src/emitter/index.js
+++ src/emitter/index.js
@@ -1,5 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var EventEmitter_1 = require("./EventEmitter");
 exports.EventEmitter = EventEmitter_1.EventEmitter;
-
 //# sourceMappingURL=index.js.map
